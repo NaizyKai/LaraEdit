c0190ae96118455d93a8afc1dfe3747e07b616f0
Derek Bourgeois <derek@ibourgeois.com>
2015-03-13 21:50:10 -0400

    Create LaraEditServiceProvider.php

---
+++ src/LaraEditServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LaraEdit\LaraEdit;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraEditServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->publishes([
+    			__DIR__.'/../public/' => public_path('vendor/laraedit'),
+		], 'public');
+
+		$this->loadViewsFrom(__DIR__.'/Views', 'laraedit');
+		
+		include __DIR__.'/routes.php';
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+	
+	}
+
+}
