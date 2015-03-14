692933c9be1ebb675e6b76d06213e444ed5a9a81
Derek Bourgeois <derek@ibourgeois.com>
2015-03-13 21:53:43 -0400

    Create navbar.blade.php

---
+++ src/Views/partials/navbar.blade.php
@@ -0,0 +1,11 @@
+@section('navbar')
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/laraedit">
+                <img alt="LaraEdit" src="https://raw.githubusercontent.com/laravel/laravel.com/master/public/assets/img/laravel-logo-white.png">
+            </a>
+        </div>
+    </div>
+</nav>
+@show
