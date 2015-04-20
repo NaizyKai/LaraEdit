57c8875e6a29b2e3b83dba60a37f6d63afd21253
ibourgeois <derek@ibourgeois.com>
2015-04-20 02:15:12 -0400

    update repo

---
+++ src/Views/partials/global/quicknav.blade.php
@@ -0,0 +1,12 @@
+@section('quicknav')
+<div class="quicknav">
+	<ul>
+		<li class="quicknav-button">
+			<i class="fa fa-code"></i>
+		</li>
+		<li class="quicknav-button">
+			<i class="fa fa-terminal"></i>
+		</li>
+	</ul>
+</div>
+@show
