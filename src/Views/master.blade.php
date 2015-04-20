f0470581bc06463d32ccc91bca17735ae20bafdb
ibourgeois <derek@ibourgeois.com>
2015-04-20 01:35:01 -0400

    update repo

--- src/Views/master.blade.php
+++ src/Views/master.blade.php
@@ -7,9 +7,11 @@
         
         <title>LaraEdit</title>
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="/vendor/laraedit/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/vendor/laraedit/css/bootstrap-theme.min.css">
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        
         <link rel="stylesheet" href="/vendor/laraedit/css/laraedit.css">
         
         <!--[if lt IE 9]>
@@ -24,6 +26,6 @@
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="https://code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <script src="/vendor/laraedit/js/bootstrap.min.js"></script>
   </body>
 </html>
