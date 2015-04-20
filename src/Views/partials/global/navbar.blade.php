2fa229afecc7af6c00c7f0b12bb865cb0ac901e5
ibourgeois <derek@ibourgeois.com>
2015-04-20 01:52:09 -0400

    update repo

--- src/Views/partials/global/navbar.blade.php
+++ src/Views/partials/global/navbar.blade.php
@@ -1,7 +1,27 @@
 @section('navbar')
-<ul>
-	<li>Home</li>
-	<li>Editor</li>
-	<li>Terminal</li>
-</ul>
+<div class="container-fluid">
+	<div class="row">
+		<nav class="navbar navbar-ineverse navbar-fixed-top alizarin" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-header">
+	      			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+	      			</button>
+	      			<a class="navbar-brand" href="/"><i class="fa fa-book"></i> LaraEdit</a>
+	    		</div>
+				<div class="collapse navbar-collapse" id="navbar">
+		    		<ul class="nav navbar-nav">
+			    
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+					   
+	            	</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+</div>
 @show
